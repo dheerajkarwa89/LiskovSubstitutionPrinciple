3d33569344d28e1df9535e658b56c1e3bbac3288
Dheeraj <dheerajkarwa89@gmail.com>
2024-10-08 19:44:34 +0530

    Add project files.

---
+++ After.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiskovSubstitutionPrinciple
+{
+    public abstract class _Bird
+    {        
+    }
+
+    public interface IFlyingBird
+    {
+        void Fly();
+    }
+
+    public class _Sparrow : _Bird, IFlyingBird
+    {
+        public void Fly()
+        { 
+        }
+    }
+
+    public class _Penguin : _Bird
+    {
+        //no fly method adhering to LSP
+    }
+}
---
+++ Before.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiskovSubstitutionPrinciple
+{
+    public class Bird
+    {
+        public virtual void Fly()
+        {
+        }
+    }
+
+    public class Sparrow : Bird
+    {
+        public override void Fly()
+        {
+        }
+    }
+
+    public class Penguin : Bird
+    {
+        //Below fly is not applicable to penguin
+        public override void Fly()
+        {
+        }
+    }
+}
